377720e601df8efb82a81538c4afbaea828e8efa
Skeeve2 <skeevyskeeve@gmail.com>
2019-04-23 15:27:09 -0400

    Refactored slightly

--- global.lua
+++ global.lua
@@ -2,35 +2,38 @@
 
 --[[ The onLoad event is called after the game save finishes loading. --]]
 function onLoad()
-  splash_screen_animation()
-  button_animation()
+  start_sequence() -- Comment this out to disable start sequence
   play = true
 end
 
 local framebg = 0
 local framebut = 0
 
-function splash_screen_animation()
-  if play == false then return nil end
-  framebg = framebg + 1
-  if framebg == 12 then framebg = 1 end
-  UI.setAttribute("frame","image", tostring(framebg))
-  Wait.time(splash_screen_animation, 0.11)
-end
+function start_sequence()
+  local function splash_screen_animation()
+    if play == false then return nil end
+    framebg = framebg + 1
+    if framebg == 12 then framebg = 1 end
+    UI.setAttribute("frame", "image", tostring(framebg))
+    Wait.time(splash_screen_animation, 0.11)
+  end
 
-function button_animation()
-  if play == false then return nil end
+  local function button_animation()
+    if play == false then return nil end
     framebut = framebut + 1
-      if framebut == 3 then framebut = 1 end
-    UI.setAttribute("frame2","image", "select" .. tostring(framebut))
-    UI.setAttribute("frame3","image", "select" .. tostring(framebut))
-  Wait.time(button_animation, 0.75)
+    if framebut == 3 then framebut = 1 end
+    UI.setAttribute("frame2", "image", "select" .. tostring(framebut))
+    UI.setAttribute("frame3", "image", "select" .. tostring(framebut))
+    Wait.time(button_animation, 0.75)
+  end
+  splash_screen_animation()
+  button_animation()
 end
 
 function startgame()
   local function stop()
-  play = false
+    play = false
   end
   UI.hide("splashscreen")
-  Wait.time(stop,1) -- Keeps animation playing until panel is fully gone
+  Wait.time(stop, 1) -- Keeps animation playing until panel is fully gone
 end
